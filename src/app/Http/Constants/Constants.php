7be27d3916942b8d66b9d2bedd12f841a4d484cb
mohammad <usermp76@gmail.com>
2024-05-21 09:42:46 +0330

    Refactor Credential

--- src/app/Http/Constants/Constants.php
+++ src/app/Http/Constants/Constants.php
@@ -14,7 +14,9 @@ class Constants
     const ERROR_DELETE = "Delete Error";
 
     const SUCCESS_LOGIN = "Login successful. Welcome to the system!";
+    const ERROR_REGISTER_FIRST = "User not found please register first";
     const ERROR_LOGIN   = "Invalid credentials. The provided username or password does not match our records.";
+    const ERROR_LOGIN_OTP   = "Invalid credentials. The provided OTP does not match our records.";
     const EMAIL_DISABLE = 'Email login is disabled.';
     const OTP_EXPIRED = 'The OTP has expired. Please request a new one.';
     const OTP_STILL_VALID = 'A valid OTP already exists. Please use the existing OTP or wait until it expires.';
--- src/app/Http/Controllers/Auth/CredentialsController.php
+++ src/app/Http/Controllers/Auth/CredentialsController.php
@@ -2,9 +2,11 @@
 
 namespace Usermp\LaravelLogin\app\Http\Controllers\Auth;
 
-
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Carbon;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
+use Usermp\LaravelLogin\app\Models\Otp;
 use Usermp\LaravelLogin\app\Models\User;
 use Usermp\LaravelLogin\app\Http\Services\Response;
 use Usermp\LaravelLogin\app\Http\Constants\Constants;
@@ -12,39 +14,85 @@ use Usermp\LaravelLogin\app\Http\Requests\Auth\CredentialsRequest;
 
 class CredentialsController extends Controller
 {
-    public function credential(CredentialsRequest $request)
+    /**
+     * Handle the incoming credential request.
+     *
+     * This method handles the authentication request based on the type
+     * specified in the request. It can either authenticate using OTP or password.
+     *
+     * @param CredentialsRequest $request
+     * @return JsonResponse
+     */
+    public function credential(CredentialsRequest $request): JsonResponse
     {
         $validated = $request->validated();
 
-        if($validated['type'] == "otp")
-        {
+        if ($validated['type'] === 'otp') {
             return $this->handleWithOtp($validated);
         }
 
         return $this->handleWithPassword($validated);
     }
 
-    private function handleWithOtp(array $validated)
+    /**
+     * Handle authentication using OTP.
+     *
+     * This method verifies the OTP provided in the request. If the OTP is valid and not expired,
+     * it retrieves the user associated with the OTP and returns a success response with an access token.
+     *
+     * @param array $validated
+     * @return JsonResponse
+     */
+    private function handleWithOtp(array $validated): JsonResponse
     {
+        $otpRecord = Otp::where('username', $validated['username'])
+                        ->where('type', 'Login')
+                        ->latest('expired_at')
+                        ->first();
 
-    }
-    private function handleWithPassword(array $validated)
-    {
-        $username = $validated['username'];
-        $user = User::where("phone", $username)->first();
-
-        if( ! $user )
-            return Response::error(Constants::ERROR_LOGIN,401);
-
-        $credential = [
-            "phone"    => $validated['username'],
-            "password" => $validated['credentials'],
-        ];
-        if (Auth::attempt($credential))
-        {
-            $user['token'] = $user->createToken('Laravel Password Grant Client')->accessToken;
-            return Response::success("",$user);
+        if (!$otpRecord || Carbon::now()->greaterThan($otpRecord->expired_at)) {
+            return Response::error(Constants::OTP_EXPIRED);
         }
-        return Response::error(Constants::ERROR_LOGIN);
+
+        if ($otpRecord->token !== $validated['credentials']) {
+            return Response::error(Constants::ERROR_LOGIN_OTP);
+        }
+
+        $user = User::where('phone', $otpRecord->username)->first();
+        if (!$user) {
+            return Response::error(Constants::ERROR_REGISTER_FIRST);
+        }
+
+        $user['token'] = $user->createToken('Laravel Password Grant Client')->accessToken;
+        return Response::success("", $user);
+    }
+
+    /**
+     * Handle authentication using password.
+     *
+     * This method verifies the password provided in the request. If the credentials are valid,
+     * it retrieves the user and returns a success response with an access token.
+     *
+     * @param array $validated
+     * @return JsonResponse
+     */
+    private function handleWithPassword(array $validated): JsonResponse
+    {
+        $credentials = [
+            'phone' => $validated['username'],
+            'password' => $validated['credentials'],
+        ];
+
+        if (!Auth::attempt($credentials)) {
+            return Response::error(Constants::ERROR_LOGIN, 401);
+        }
+
+        $user = User::where('phone', $validated['username'])->first();
+        if (!$user) {
+            return Response::error(Constants::ERROR_LOGIN, 401);
+        }
+
+        $user['token'] = $user->createToken('Laravel Password Grant Client')->accessToken;
+        return Response::success("", $user);
     }
 }
